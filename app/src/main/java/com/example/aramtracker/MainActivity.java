0169256b0d57a97f4a86843e6050b16b38fe188c
Hornisslaw <rkaczmarski.1998@gmail.com>
2022-04-22 16:44:04 +0200

    fix basic user+mmr display

--- app/src/main/java/com/example/aramtracker/MainActivity.java
+++ app/src/main/java/com/example/aramtracker/MainActivity.java
@@ -4,24 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
-import android.widget.EditText;
+import android.os.StrictMode;
 import android.widget.TextView;
-
+import org.json.JSONException;
 import java.io.IOException;
-import java.sql.SQLOutput;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         setContentView(R.layout.activity_main);
         UserStatistics user = null;
         try {
             user = new UserStatistics("horniss");
             TextView t = (TextView) findViewById(R.id.test1);
-            t.setText(user.getNickname());
-        } catch (IOException e) {
+            t.setText(user.getNickname() + ": " + user.getAramMMR());
+        } catch (IOException | JSONException e) {
             e.printStackTrace();
         }
     }
--- app/src/main/java/com/example/aramtracker/UserStatistics.java
+++ app/src/main/java/com/example/aramtracker/UserStatistics.java
@@ -1,22 +1,42 @@
 package com.example.aramtracker;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 public class UserStatistics {
     public final String nickname;
+    public final String aramMMR;
     public String data;
-    public UserStatistics(String nickname) throws IOException {
+
+    public UserStatistics(String nickname) throws IOException, JSONException {
         this.nickname = nickname;
-        try {
-            this.data = GetApi.getFromAram(nickname);
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
+        this.data = GetApi.getFromAram(nickname);
+        this.aramMMR = processAramMMR(this.data);
+//        try {
+//            this.data = GetApi.getFromAram(nickname);
+//        }
+//        catch (IOException e)
+//        {
+//            e.printStackTrace();
+//        }
     }
+
     public String getNickname()
     {
-        return "https://eune.whatismymmr.com/api/v1/summoner?name=" + this.nickname;
+        return nickname;
+    }
+
+    public String getAramMMR()
+    {
+        return aramMMR;
+    }
+
+    public String processAramMMR(String data) throws JSONException {
+        JSONObject aram = (new JSONObject(data)).getJSONObject("ARAM");
+        String aramMMR = aram.getString("avg");
+
+        return aramMMR;
     }
 }
